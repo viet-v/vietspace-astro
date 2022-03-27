23de7b8ba79bebab7c4b1d2ed6c0d8687e59a5df
viet_v <vohoangviet459@gmail.com>
2022-03-27 09:22:16 +0700

    Migrated index elements into components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
 import { defineConfig } from 'astro/config';
   import react from '@astrojs/react';
 // https://astro.build/config
-export default defineConfig({
-  integrations: [
-    react(),
-  ]
+
+
+export default /** @type {import('astro').AstroUserConfig} */ ({
+  // all options are optional; these values are the defaults
+  projectRoot: './',
+  public: './public/',
+  dist: './dist/',
+  src: './src/',
+  pages: './src/pages/',
+  renderers: [
+    '@astrojs/renderer-react'
+  ],
+  vite: {},
+  devOptions: {
+    port: 3000,
+  }
 });
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,99 +2,99 @@
 export default function Projects () {
     return (
         <>
-            <section class="projects" id="projects">
-			    <div class="wrapper">
-                    <h2 class="section-title">PROJECTS</h2>
-                    <div class="project">
-                        <div class="project-board">
-                            <div class="content">
-                                <p class="project-name">Soundwave Music Player</p>
-                                <div class="project1-img"></div>
-                                <p class="project-description">Minimal music player can visualize audio and change random background.</p>
-                                <p class="project-skills">Skills: HTML, CSS, JavaScript, API</p>
-                                <p class="project-tools">Tools: Photoshop, VS Code</p>
+            <section className="projects" id="projects">
+			    <div className="wrapper">
+                    <h2 className="section-title">PROJECTS</h2>
+                    <div className="project">
+                        <div className="project-board">
+                            <div className="content">
+                                <p className="project-name">Soundwave Music Player</p>
+                                <div className="project1-img"></div>
+                                <p className="project-description">Minimal music player can visualize audio and change random background.</p>
+                                <p className="project-skills">Skills: HTML, CSS, JavaScript, API</p>
+                                <p className="project-tools">Tools: Photoshop, VS Code</p>
                             </div>
-                            <div class="project-button">
+                            <div className="project-button">
                                 <a href="https://viet-v.github.io/soundwave-music-player/" target="_blank">Site</a>
                                 <a href="https://github.com/viet-v/soundwave-music-player" target="_blank">Code</a>
                             </div>
                         </div>
                     </div>
 
-                    <div class="project">
-                        <div class="project-board">
-                            <div class="content">
-                                <p class="project-name">Better Health</p>
-                                <div class="project2-img"></div>
-                                <p class="project-description">A blog sharing health knowledge and helpful habits.</p>
-                                <p class="project-skills">Skills: WordPress, FTP, SEO</p>
-                                <p class="project-tools">Tools: WordPress, Photoshop</p>
+                    <div className="project">
+                        <div className="project-board">
+                            <div className="content">
+                                <p className="project-name">Better Health</p>
+                                <div className="project2-img"></div>
+                                <p className="project-description">A blog sharing health knowledge and helpful habits.</p>
+                                <p className="project-skills">Skills: WordPress, FTP, SEO</p>
+                                <p className="project-tools">Tools: WordPress, Photoshop</p>
                             </div>
-                            <div class="project-button">
+                            <div className="project-button">
                                 <a href="https://www.vietspace.me/giupbankhoehon/" target="_blank">Site</a>
                                 <a href="https://github.com/viet-v/giup-ban-khoe-hon" target="_blank">Code</a>
                             </div>
                         </div>
                     </div>
 
-                    <div class="project">
-                        <div class="project-board">
-                            <div class="content">
-                                <p class="project-name">Sticky Notes</p>
-                                <div class="project3-img"></div>
-                                <p class="project-description">A web app built with multiple components helps you create, edit, delete and search for notes.</p>
-                                <p class="project-skills">Skill: React</p>
-                                <p class="project-tools">Tool: CodeSandbox</p>
+                    <div className="project">
+                        <div className="project-board">
+                            <div className="content">
+                                <p className="project-name">Sticky Notes</p>
+                                <div className="project3-img"></div>
+                                <p className="project-description">A web app built with multiple components helps you create, edit, delete and search for notes.</p>
+                                <p className="project-skills">Skill: React</p>
+                                <p className="project-tools">Tool: CodeSandbox</p>
                             </div>
-                            <div class="project-button">
+                            <div className="project-button">
                                 <a href="https://4gpep.csb.app/" target="_blank">Site</a>
                                 <a href="https://codesandbox.io/s/sticky-notes-app-4gpep?file=/src/App.js" target="_blank">Code</a>
                             </div>
                         </div>
                     </div>
 
-                    <div class="project">
-                        <div class="project-board">
-                            <div class="content">
-                                <p class="project-name">GitHub Repo Gallery</p>
-                                <div class="project4-img"></div>
-                                <p class="project-description">Uses GitHub’s API to pull data from GitHub profile. Click on each repo for more details.</p>
-                                <p class="project-skills">Skills: JavaScript, REST API</p>
-                                <p class="project-tools">Tool: GitHub Pages</p>
+                    <div className="project">
+                        <div className="project-board">
+                            <div className="content">
+                                <p className="project-name">GitHub Repo Gallery</p>
+                                <div className="project4-img"></div>
+                                <p className="project-description">Uses GitHub’s API to pull data from GitHub profile. Click on each repo for more details.</p>
+                                <p className="project-skills">Skills: JavaScript, REST API</p>
+                                <p className="project-tools">Tool: GitHub Pages</p>
                             </div>
-                            <div class="project-button">
+                            <div className="project-button">
                                 <a href="https://viet-v.github.io/github-repo-gallery/" target="_blank">Site</a>
                                 <a href="https://github.com/viet-v/github-repo-gallery" target="_blank">Code</a>
                             </div>
                         </div>
                     </div>
 
-                    <div class="project">
-                        <div class="project-board">
-                            <div class="content">
-                                <p class="project-name">Guess The Word game</p>
-                                <div class="project5-img"></div>
-                                <p class="project-description">Simple game tests out player's vocabulary strength and flexibility.</p>
-                                <p class="project-skills">Skills: JavaScript, REST API</p>
-                                <p class="project-tools">Tool: GitHub Pages</p>
+                    <div className="project">
+                        <div className="project-board">
+                            <div className="content">
+                                <p className="project-name">Guess The Word game</p>
+                                <div className="project5-img"></div>
+                                <p className="project-description">Simple game tests out player's vocabulary strength and flexibility.</p>
+                                <p className="project-skills">Skills: JavaScript, REST API</p>
+                                <p className="project-tools">Tool: GitHub Pages</p>
                             </div>
-                            <div class="project-button">
+                            <div className="project-button">
                                 <a href="https://viet-v.github.io/guess-the-word/" target="_blank">Site</a>
                                 <a href="https://github.com/viet-v/guess-the-word" target="_blank">Code</a>
                             </div>
                         </div>
                     </div>
 
-                    <div class="project" id="project5">
-                        <div class="project-board">
-                            <div class="content">
-                                <p class="project-name">Unplugged</p>
-                                <div class="project6-img"></div>
-                                <p class="project-description">Commercial multi-page responsive website.</p>
-                                <p class="project-skills">Skills: HTML, CSS Flexbox</p>
-                                <p class="project-tools">Tools: Chrome DevTools, VS Code</p>
+                    <div className="project" id="project5">
+                        <div className="project-board">
+                            <div className="content">
+                                <p className="project-name">Unplugged</p>
+                                <div className="project6-img"></div>
+                                <p className="project-description">Commercial multi-page responsive website.</p>
+                                <p className="project-skills">Skills: HTML, CSS Flexbox</p>
+                                <p className="project-tools">Tools: Chrome DevTools, VS Code</p>
                             </div>
-                            <div class="project-button">
+                            <div className="project-button">
                                 <a href="https://viet-v.github.io/unplugged-project/" target="_blank">Site</a>
                                 <a href="https://github.com/viet-v/unplugged-project" target="_blank">Code</a>
                             </div>
